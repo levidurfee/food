f1d13b51b9a614bded792b6488f665a771d6c056
Levi Durfee <levi.durfee@gmail.com>
2018-02-22 16:10:50 -0500

    Add files

---
+++ options.js
@@ -0,0 +1,17 @@
+var options = [
+    'McDonalds',
+    'Fats',
+    'Subway',
+    'Hardees',
+    'Chinese',
+    'Pizza',
+    'Fazolis',
+    'Roccos',
+    'Jewel City',
+    'Ale House',
+    'Clean Eats',
+    'Buddys',
+    'Press Club',
+    'Margaritas',
+    'La Famiglia'
+];
